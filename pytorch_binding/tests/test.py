9a2f718ac03588a2d91ffbce57e06350e0fe4fee
SeanNaren <seannaren@hotmail.com>
2017-01-26 11:47:32 +0000

    Updated tests

--- pytorch_binding/tests/test.py
+++ pytorch_binding/tests/test.py
@@ -1,86 +1,107 @@
 import torch
-import torch.cuda
-
 import warpctc_pytorch as warp_ctc
 
-probs = torch.FloatTensor([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]]).transpose(0, 1).contiguous()
 
-grads = torch.zeros(probs.size())
-labels = torch.IntTensor([3, 3])
-label_sizes = torch.IntTensor([2])
+def simple_test():
+    probs = torch.FloatTensor([[[0.1, 0.6, 0.1, 0.1, 0.1], [0.1, 0.1, 0.6, 0.1, 0.1]]]).transpose(0, 1).contiguous()
+    grads = torch.zeros(probs.size())
+    labels = torch.IntTensor([1, 2])
+    label_sizes = torch.IntTensor([2])
+    sizes = torch.IntTensor(probs.size(1)).fill_(probs.size(0))
+    minibatch_size = probs.size(1)
+    costs = torch.zeros(minibatch_size)
+    warp_ctc.cpu_ctc(probs,
+                     grads,
+                     labels,
+                     label_sizes,
+                     sizes,
+                     minibatch_size,
+                     costs)
+    print('CPU_cost: %f' % costs.sum())
+    probs = probs.clone().cuda()
+    grads = torch.zeros(probs.size()).cuda()
+    costs = torch.zeros(minibatch_size)
+    warp_ctc.gpu_ctc(probs,
+                     grads,
+                     labels,
+                     label_sizes,
+                     sizes,
+                     minibatch_size,
+                     costs)
+    print('GPU_cost: %f' % costs.sum())
+    print(grads.view(grads.size(0) * grads.size(1), grads.size(2)))
 
-sizes = torch.IntTensor(probs.size(1)).fill_(probs.size(0))
-minibatch_size = probs.size(1)
-costs = torch.zeros(1)
-warp_ctc.cpu_ctc(probs,
-                 grads,
-                 labels,
-                 label_sizes,
-                 sizes,
-                 minibatch_size,
-                 costs)
 
-print(costs.sum())
+def medium_test(multiplier):
+    probs = torch.FloatTensor([
+        [[0.1, 0.6, 0.1, 0.1, 0.1], [0.1, 0.1, 0.6, 0.1, 0.1]],
+        [[0.6, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.5, 0.2, 0.1]]
+    ]).contiguous() * multiplier
 
-probs = torch.FloatTensor([[[-5, -4, -3, -2, -1], [-10, -9, -8, -7, -6], [-15, -14, -13, -12, -11]]]) \
-    .transpose(0, 1).contiguous()
+    grads = torch.zeros(probs.size())
+    labels = torch.IntTensor([1, 2, 1, 2])
+    label_sizes = torch.IntTensor([2, 2])
+    sizes = torch.IntTensor([2, 2])
+    minibatch_size = probs.size(1)
+    costs = torch.zeros(minibatch_size)
+    warp_ctc.cpu_ctc(probs,
+                     grads,
+                     labels,
+                     label_sizes,
+                     sizes,
+                     minibatch_size,
+                     costs)
+    print('CPU_cost: %f' % costs.sum())
+    probs = probs.clone().cuda()
+    grads = torch.zeros(probs.size()).cuda()
+    costs = torch.zeros(minibatch_size)
+    warp_ctc.gpu_ctc(probs,
+                     grads,
+                     labels,
+                     label_sizes,
+                     sizes,
+                     minibatch_size,
+                     costs)
+    print('GPU_cost: %f' % costs.sum())
+    print(grads.view(grads.size(0) * grads.size(1), grads.size(2)))
 
-grads = torch.zeros(probs.size())
-labels = torch.IntTensor([2, 3])
-label_sizes = torch.IntTensor([2])
 
-sizes = torch.IntTensor(probs.size(1)).fill_(probs.size(0))
-minibatch_size = probs.size(1)
-costs = torch.zeros(1)
-warp_ctc.cpu_ctc(probs,
-                 grads,
-                 labels,
-                 label_sizes,
-                 sizes,
-                 minibatch_size,
-                 costs)
+def empty_label_test():
+    probs = torch.FloatTensor([
+        [[0.1, 0.6, 0.1, 0.1, 0.1], [0.1, 0.1, 0.6, 0.1, 0.1]],
+        [[0.6, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.5, 0.2, 0.1]]
+    ]).contiguous()
 
-print(costs.sum())
+    grads = torch.zeros(probs.size())
+    labels = torch.IntTensor([1, 2])
+    label_sizes = torch.IntTensor([2, 0])
+    sizes = torch.IntTensor([2, 2])
+    minibatch_size = probs.size(1)
+    costs = torch.zeros(minibatch_size)
+    warp_ctc.cpu_ctc(probs,
+                     grads,
+                     labels,
+                     label_sizes,
+                     sizes,
+                     minibatch_size,
+                     costs)
+    print('CPU_cost: %f' % costs.sum())
+    probs = probs.clone().cuda()
+    grads = torch.zeros(probs.size()).cuda()
+    costs = torch.zeros(minibatch_size)
+    warp_ctc.gpu_ctc(probs,
+                     grads,
+                     labels,
+                     label_sizes,
+                     sizes,
+                     minibatch_size,
+                     costs)
+    print('GPU_cost: %f' % costs.sum())
+    print(grads.view(grads.size(0) * grads.size(1), grads.size(2)))
 
-probs = torch.FloatTensor([
-    [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]],
-    [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-    [[-5, -4, -3, -2, -1], [-10, -9, -8, -7, -6], [-15, -14, -13, -12, -11]]
-]).transpose(0, 1).contiguous()
-
-grads = torch.zeros(probs.size())
-labels = torch.IntTensor([1, 3, 3, 2, 3])
-label_sizes = torch.IntTensor([1, 2, 2])
-
-sizes = torch.IntTensor([1, 3, 3])
-minibatch_size = probs.size(1)
-costs = torch.zeros(3)
-warp_ctc.cpu_ctc(probs,
-                 grads,
-                 labels,
-                 label_sizes,
-                 sizes,
-                 minibatch_size,
-                 costs)
-
-print(costs.sum())
-
-probs = torch.FloatTensor([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]]).transpose(0,
-                                                                                                 1).contiguous().cuda()
-
-grads = torch.zeros(probs.size()).cuda()
-labels = torch.IntTensor([3, 3])
-label_sizes = torch.IntTensor([2])
-
-sizes = torch.IntTensor(probs.size(1)).fill_(probs.size(0))
-minibatch_size = probs.size(1)
-costs = torch.zeros(1)
-warp_ctc.gpu_ctc(probs,
-                 grads,
-                 labels,
-                 label_sizes,
-                 sizes,
-                 minibatch_size,
-                 costs)
-
-print(costs.sum())
+simple_test()
+medium_test(1.0)
+print("Stability test")
+medium_test(200.0)  # test SM stability if compiled with USE_NSM this will not have nans
+print("Empty label test")
+empty_label_test()
